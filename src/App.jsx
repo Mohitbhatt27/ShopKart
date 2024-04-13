9587e874f534773018ea3b1febc02acee64db22b
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-13 14:17:29 +0530

    configured ReactStrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   return (
     <>
       <div>
         <h1>ShopKart</h1>
+        <Button />
       </div>
     </>
   )
