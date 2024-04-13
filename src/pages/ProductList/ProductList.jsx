c91fca126403c0f18701a05f1b72999a1698f138
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-13 16:52:13 +0530

    Added ProductDetails Page

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,8 +1,10 @@
+// CSS import
 import "./ProductList.css";
 
 // Image import
 import ProductImage from "../../assets/product.jpg";
 import ProductBox from "../../components/ProductBox/ProductBox";
+import FilterProducts from "../../components/FilterProducts/FilterProducts";
 
 function ProductList() {
   return (
@@ -10,6 +12,8 @@ function ProductList() {
       <div className="row">
         <h2 className="product-list-title text-center">All Products</h2>
         <div className="product-list-wrapper d-flex flex-row">
+          <FilterProducts />
+
           {/* list of products */}
           <div className="product-list-box" id="productList">
             <ProductBox
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -4,13 +4,14 @@ import { Route, Routes } from "react-router";
 import Home from "../pages/Home/Home";
 import Error from "../pages/Error/Error";
 import ProductList from "../pages/ProductList/ProductList";
-
+import ProductDetails from "../pages/ProductDetails/ProductDetails";
 function MainRoutes() {
 
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<ProductList />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
             <Route path="*" element={<Error />}/>
         </Routes>
     )
